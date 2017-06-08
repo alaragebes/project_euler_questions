a63942b5b198def05c1abe88f99bcaa6539ce87c
alaragebes <alaragb@gmail.com>
2017-06-08 15:16:50 +0300

    addition to code projecteulerquestion3

--- projecteulerquestion3.py
+++ projecteulerquestion3.py
@@ -1,4 +1,9 @@
 # find the numbers the factors
+""""
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
 
 def find_factors (n = 600851475143) :
     elements = []
